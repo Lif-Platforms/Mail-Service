6aafe1e5336a61a73b8c9edbe0e23341481cbaed
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-12-03 22:30:21 -0500

    Add new credentials system

--- src/utils/database_interface.py
+++ src/utils/database_interface.py
@@ -1,4 +1,6 @@
 import sqlite3
+import mysql.connector
+from mysql.connector.constants import ClientFlag
 
 global configuration
 configuration = None
@@ -7,6 +9,26 @@ configuration = None
 def set_config(config):
     global configuration
     configuration = config
+
+async def connect_to_database():
+    # async Define configurations
+    mysql_configs = {
+        "host": configuration['mysql-host'],
+        "port": configuration['mysql-port'],
+        "user": configuration['mysql-user'],
+        "password": configuration['mysql-password'],
+        "database": configuration['mysql-database'], 
+    }
+
+    # Check if SSL is enabled
+    if configuration['mysql-ssl']:
+        # Add ssl configurations to connection
+        mysql_configs['client_flags'] = [ClientFlag.SSL]
+        mysql_configs['ssl_ca'] = configuration['mysql-cert-path']
+
+    conn = mysql.connector.connect(**mysql_configs)
+    
+    return conn
 
 def add_to_ringer_waitlist(email: str):
     # Connects to database
@@ -35,3 +57,85 @@ def fetch_all_ringer_waitlist():
     items = c.fetchall()
 
     return items
+
+class credentials:
+    async def create_credentials(name: str, client_id: str, client_secret_hash: str, secret_salt: str):
+        conn = await connect_to_database()
+        cursor = conn.cursor()
+
+        # Add credentials to database
+        cursor.execute(
+            "INSERT INTO credentials (name, client_id, client_secret, secret_salt) VALUES (%s, %s, %s, %s);", 
+            (name, client_id, client_secret_hash, secret_salt)
+        )
+        conn.commit()
+        conn.close()
+
+    async def remove_credentials(client_id: str):
+        conn = await connect_to_database()
+        cursor = conn.cursor()
+
+        # Delete credentials
+        cursor.execute("DELETE FROM credentials WHERE client_id = %s;", (client_id,))
+
+        # Delete all permissions associated with those credentials
+        cursor.execute("DELETE FROM permissions WHERE client_id = %s;", (client_id,))
+
+        conn.commit()
+        conn.close()
+
+    async def get_credentials(client_id):
+        conn = await connect_to_database()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT name, client_id, client_secret, secret_salt FROM credentials WHERE client_id = %s;",
+                       (client_id,))
+        data = cursor.fetchone()
+        conn.close()
+
+        if data:
+            return {
+                "name": data[0],
+                "client_id": data[1],
+                "client_secret_hash": data[2],
+                "secret_salt": data[3]
+            }
+        else:
+            return None
+
+class permissions:
+    async def add_permissions(permissions: list, client_id: str):
+        conn = await connect_to_database()
+        cursor = conn.cursor()
+
+        for permission in permissions:
+            cursor.execute("INSERT INTO permissions (client_id, permission_node) VALUES (%s, %s);",
+                           (client_id, permission))
+        conn.commit()
+        conn.close()
+
+    async def remove_permissions(permissions: list, client_id: str):
+        conn = await connect_to_database()
+        cursor = conn.cursor()
+
+        for permission in permissions:
+            cursor.execute("DELETE FROM permissions WHERE client_id = %s AND permission_node = %s;",
+                           (client_id, permission))
+        conn.commit()
+        conn.close()
+
+    async def get_permissions(client_id: str):
+        conn = await connect_to_database()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT permission_node FROM permissions WHERE client_id = %s;",
+                       (client_id,))
+        data = cursor.fetchall()
+        conn.close()
+
+        format_data = []
+
+        for node in data:
+            format_data.append(node[0])
+
+        return format_data
